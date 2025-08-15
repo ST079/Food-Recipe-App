52aae34373684b5455f8b2bf0851f7061a0967db
Sujan Tamang <suzanyba079@gmail.com>
2025-08-15 13:43:34 +0545

    server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
+const Router = require("./routes/recipe")
 
 const PORT = process.env.PORT || 5000;
 
+
+app.use("/recipe",Router);
 
 app.listen(PORT, ()=>{
     console.log(`App is running at port ${PORT}`)
---
+++ server/routes/recipe.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const {
+  getRecipes,
+  getRecipe,
+  addRecipe,
+  editRecipe,
+  deleteRecipe,
+} = require("./recipeController");
+const router = express.Router();
+
+router.get("/", getRecipes); // get all the available recipes
+
+router.get("/:id", getRecipe); // get recipe by id
+
+router.post("/", addRecipe); // to add the recipe
+
+router.put("/:id", editRecipe); // to edit the recipe by id
+
+router.delete("/:id", deleteRecipe); // to delet the recipe
+module.exports = router;
